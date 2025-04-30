c3d484630a6f5525e81e5c18995231589c5a04ec
Matthias Vach <matthias.vach@sap.com>
2025-04-30 15:12:56 +0200

    Further fixes (maybe)
    Co-authored-by: Nicolas Bender <nicolas.bender@sap.com>

---
+++ renovate-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    username: 'renovate-release',
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+    onboarding: false,
+    platform: 'github',
+    forkProcessing: 'enabled',
+    dryRun: 'full',
+    repositories: ['nicolasbender/nodejs']
+}
